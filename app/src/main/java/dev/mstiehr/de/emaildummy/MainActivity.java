136e4d9d7c55ef8c2b8443c2f7e869ee95bde7cc
mstiehr-dev@github.com <m.stiehr@gmail.com>
2017-05-19 16:27:02 +0200

    add room persistence library

--- app/src/main/java/dev/mstiehr/de/emaildummy/MainActivity.java
+++ app/src/main/java/dev/mstiehr/de/emaildummy/MainActivity.java
@@ -3,15 +3,26 @@ package dev.mstiehr.de.emaildummy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+    ListView mListView;
+    List<String> subjects = new ArrayList<>();
 
     @Override
     protected void onCreate (Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(android.R.id.list);
+        mListView.setEmptyView(findViewById(android.R.id.empty));
+        mListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, subjects));
     }
 }
